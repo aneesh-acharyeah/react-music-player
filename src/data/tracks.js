9f6c87cf01d0357deae71ce63ee6b99b77d242ce
Aneesh <aneeshg2001@gmail.com>
2025-07-07 09:39:34 +0530

    update1

---
+++ src/data/tracks.js
@@ -0,0 +1,22 @@
+const tracks = [
+  {
+    title: "Lost Sky - Fearless",
+    artist: "Lost Sky",
+    audioSrc: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3",
+    image: "https://i1.sndcdn.com/artworks-000640223862-gy5ljh-t500x500.jpg"
+  },
+  {
+    title: "Alan Walker - Fade",
+    artist: "Alan Walker",
+    audioSrc: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3",
+    image: "https://i1.sndcdn.com/artworks-000637203703-1p4k9u-t500x500.jpg"
+  },
+  {
+    title: "Elektronomia - Energy",
+    artist: "Elektronomia",
+    audioSrc: "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3",
+    image: "https://i1.sndcdn.com/artworks-000563947035-s9zqjf-t500x500.jpg"
+  }
+];
+
+export default tracks;
